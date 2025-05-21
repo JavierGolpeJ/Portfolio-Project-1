162b0dc5bba1ab9cb753ebf172c75ab07a1836c1
Javier G <javiermazaba10@gmail.com>
2025-05-20 21:33:55 -0500

    add inspirational quotes

---
+++ main.js
@@ -0,0 +1,26 @@
+const inspiration = ["To be grateful is to recognize the Love of God in everything He has given us - and He has given us everything. Every breath we draw is a gift of His love, every moment of existence is a grace, for it brings with it immense graces from Him. Gratitude therefore takes nothing for granted, is never unresponsive, is constantly awakening to new wonder and to praise of the goodness of God. For the grateful person knows that God is good, not by hearsay but by experience. And that is what makes all the difference.- Thomas Merton",
+    "Write it on your heart that every day is the best day in the year. - Ralph Waldo Emerson",
+    "Never let hard lessons harden your heart; the hard lessons of life are meant to make you better, not bitter. - Roy T. Bennett, The Light in the Heart",
+    "Being happy doesn't mean that everything is perfect. It means that you've decided to look beyond the imperfections. - Gerard Way",
+    "I long to accomplish a great and noble task, but it is my chief duty to accomplish small tasks as if they were great and noble. - Helen Keller",
+    "Anything under God's control is never out of control. - Charles Swindoll",
+    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+    "You have brains in your head. You have feet on your shoes. You can steer yourself any direction you choose. - Dr. Seuss",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "The people who are crazy enough to think they can change the world are the ones who do. - Steve Jobs",
+    "Be strong and take heart, all you who hope in the Lord. — Psalm 31:24",
+    "Show me your ways, Lord, teach me your paths. — Psalm 25:4",
+    "The Lord is my strength and my shield; my heart trusts in him, and he helps me. — Psalm 28:7",
+    "The Lord is my light and my salvation; whom shall I fear? The Lord is the stronghold of my life; of whom shall I be afraid? — Psalm 27:1",
+    "The Lord is close to the brokenhearted and saves those who are crushed in spirit. — Psalm 34:18",
+    "The Lord will fight for you; you need only to be still. — Exodus 14:14",
+    "The Lord is good, a refuge in times of trouble. He cares for those who trust in him. — Nahum 1:7",
+    "The Lord is my shepherd, I lack nothing. — Psalm 23:1",
+    "The Lord is my strength and my song; he has become my salvation. — Exodus 15:2"
+]
+
+
+const collectiveWisdom = { inspiration: inspiration };
+
+console.log(collectiveWisdom.inspiration[Math.floor(Math.random() * collectiveWisdom.inspiration.length)]);
